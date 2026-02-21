049b869949ecb2b9e8e45773cb8e01d448715c46
Ormazabal Nascimento <ormazabalnascimento@gmail.com>
2026-02-21 00:17:37 -0300

    feat: adiciona hateoas ao metodo getById

--- Ada.Caixa.Aula01.WebApi/Controllers/ProdutosController.cs
+++ Ada.Caixa.Aula01.WebApi/Controllers/ProdutosController.cs
@@ -17,6 +17,48 @@ public class ProdutosController : ControllerBase
         return Ok(produtos);
     }
 
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var produto = produtos.FirstOrDefault(p => p.Id.Equals(id));
+        if(produto == null)
+            return NotFound($"Produto ID: {id} não encontrado");
+
+        var response = new
+        {
+            produto.Id,
+            produto.Nome,
+            produto.Quantidade,
+            _links = new
+            {
+                self = new { href = Url.Action("Get", new {id}), method = "GET" },
+                update = new { href = Url.Action("Update", new Produto()), method = "PUT" },
+                delete = new { href = Url.Action("Delete", new {id}), method = "DELETE" }
+            }
+        };
+        
+        return Ok(response);
+    }
+
+    [HttpPut]
+    public IActionResult Put([FromBody] Produto produto)
+    {
+        var produtoExistente = produtos.FirstOrDefault(p => p.Id.Equals(produto.Id));
+
+        if(produtoExistente != null)
+        {
+            produtoExistente.Nome = produto.Nome;
+            produtoExistente.Quantidade = produto.Quantidade;
+        }
+        else
+            return BadRequest($"Produto ID: {produto.Id} não encontrado");
+
+        var index = produtos.IndexOf(produtoExistente);
+        produtos[index] = produtoExistente;
+
+        return Ok(produtoExistente);
+    }
+
     [HttpPost]
     public IActionResult Post([FromBody] Produto produto)
     {
@@ -24,8 +66,8 @@ public class ProdutosController : ControllerBase
         return Ok(produtos);
     }
 
-    [HttpDelete]
-    public IActionResult Delete([FromBody] int id)
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
     {
         var produto = produtos.FirstOrDefault(p => p.Id.Equals(id));
 
